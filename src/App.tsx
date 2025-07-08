48ac32d127a6714c70881e388a4aae7a5e3467af
Mahan Asadi <mahan.asadiiii@gmail.com>
2025-07-08 17:25:51 +0330

    fix mobile size UI bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className="flex justify-center items-center h-dvh w-full">
-      <aside>
-
-      </aside>
+    <div className="flex justify-center items-center min-h-dvh w-full">
       <main className="flex justify-center items-start flex-col max-w-lg gap-5 p-4">
         <SearchInput value={searchTerm} onChange={setSearchTerm} />
         <TextDisplay
